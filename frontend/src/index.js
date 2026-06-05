a8e9aa168c160596c77321ed4f014f22fe56a581
maintainitbandits-netizen <maintainitbandits@gmail.com>
2026-06-04 22:30:58 -0500

    Refactor index.js to include Analytics component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-import { inject } from "@vercel/analytics";
 import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "@/index.css";
 import App from "@/App";
 
@@ -15,14 +15,13 @@ const queryClient = new QueryClient({
   },
 });
 
-inject();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
-    <Analytics />
+      <App />
+      <Analytics />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
